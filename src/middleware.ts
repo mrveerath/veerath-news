9ecc79c43363ef117dbbf152d66359f22cdedb8b
pawan <pwnkr7777@gmail.com>
2025-06-12 22:32:11 +0545

    configured again the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,22 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+const privateRoutes = ['/create-blog', '/profile']
+
+// Create a route matcher function
+const isPrivateRoute = createRouteMatcher(privateRoutes)
+
+export default clerkMiddleware((auth, req) => {
+  if (isPrivateRoute(req)) {
+    // Protect the private routes
+    auth().protect()
+  }
+})
 
 export const config = {
-    matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-    ],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 }
